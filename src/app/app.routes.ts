c10b73fe31ee910d02e018d6ca6e71d645b30aff
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2024-04-08 21:13:23 +0200

    asdfa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -20,7 +20,7 @@ export const ROUTES: Routes = [
     component: NoPermissionsComponent,
   },
   {
-    path: "dashboard",
+    path: "performance-manager/dashboard",
     loadChildren: () =>
       import("./components/dashboard/routes").then((m) => m.DASHBOARD_ROUTES),
     canActivate: [MsalGuard],
