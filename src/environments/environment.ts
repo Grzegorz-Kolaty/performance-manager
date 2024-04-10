ab5d11eceea753d90f2d03ccada811331fbd30dd
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2024-04-10 04:16:29 +0200

    finnaly, workin s hit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   MSAL: {
     CLIENT_ID: "2b1bccbe-65ef-47f2-adad-f22a38663eed",
     AUTHORITY:
-      "https://login.microsoftonline.com/common/",
+      "https://login.microsoftonline.com/1d519519-709e-43d9-927b-5c6bcdc52896",
     REDIRECT_URI: "/performance-manager/dashboard",
     POST_LOGOUT_URI: "/",
     SCOPES_ARRAY: [
